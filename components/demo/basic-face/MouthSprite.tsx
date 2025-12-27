ce0d7a69ae11573bc911250b7412de924811481b
DavidValenciaX <60826603+DavidValenciaX@users.noreply.github.com>
2025-12-27 16:50:08 -0500

    feat: Add mouth SVG assets and MouthSprite component for complete viseme display.

--- components/demo/basic-face/MouthSprite.tsx
+++ components/demo/basic-face/MouthSprite.tsx
@@ -5,81 +5,60 @@
 import { useMemo } from 'react';
 import type { WawaViseme } from '../../../hooks/demo/use-face';
 
-// Import all mouth SVGs
-import MouthAE from '../mouths/AE.svg';
-import MouthBMP from '../mouths/BMP.svg';
-import MouthCDGKNSTXYZ from '../mouths/CDGKNSTXYZ.svg';
-import MouthFV from '../mouths/FV.svg';
-import MouthL from '../mouths/L.svg';
-import MouthO from '../mouths/O.svg';
-import MouthQW from '../mouths/QW.svg';
-import MouthR from '../mouths/R.svg';
+// Import all 15 wawa-lipsync mouth SVGs
+import MouthSIL from '../mouths/SIL.svg';
+import MouthPP from '../mouths/PP.svg';
+import MouthFF from '../mouths/FF.svg';
 import MouthTH from '../mouths/TH.svg';
+import MouthDD from '../mouths/DD.svg';
+import MouthKK from '../mouths/kk.svg';
+import MouthCH from '../mouths/CH.svg';
+import MouthSS from '../mouths/SS.svg';
+import MouthNN from '../mouths/nn.svg';
+import MouthRR from '../mouths/RR.svg';
+import MouthAA from '../mouths/aa.svg';
+import MouthE from '../mouths/E.svg';
+import MouthI from '../mouths/I.svg';
+import MouthO from '../mouths/O.svg';
+import MouthU from '../mouths/U.svg';
 
 /**
- * Maps WawaViseme to the appropriate mouth sprite
+ * Maps WawaViseme directly to its corresponding mouth sprite
  */
 function getMouthSprite(viseme: WawaViseme): string {
     switch (viseme) {
-        // Silence - use closed mouth (BMP)
         case 'sil':
-            return MouthBMP;
-
-        // Bilabials P, B, M - closed/pressed lips
+            return MouthSIL;
         case 'PP':
-            return MouthBMP;
-
-        // Labiodentals F, V - teeth on lip
+            return MouthPP;
         case 'FF':
-            return MouthFV;
-
-        // Dental TH
+            return MouthFF;
         case 'TH':
             return MouthTH;
-
-        // Alveolars D, T, N - teeth visible
         case 'DD':
-            return MouthCDGKNSTXYZ;
-
-        // Velars K, G
+            return MouthDD;
         case 'kk':
-            return MouthCDGKNSTXYZ;
-
-        // Postalveolars CH, SH, J
+            return MouthKK;
         case 'CH':
-            return MouthCDGKNSTXYZ;
-
-        // Sibilants S, Z
+            return MouthCH;
         case 'SS':
-            return MouthCDGKNSTXYZ;
-
-        // Nasals/Laterals N, L
+            return MouthSS;
         case 'nn':
-            return MouthL;
-
-        // R sounds
+            return MouthNN;
         case 'RR':
-            return MouthR;
-
-        // Vowel A (open) and E
+            return MouthRR;
         case 'aa':
+            return MouthAA;
         case 'E':
-            return MouthAE;
-
-        // Vowel I (EE) - wide/spread
+            return MouthE;
         case 'I':
-            return MouthCDGKNSTXYZ;
-
-        // Vowel O - rounded
+            return MouthI;
         case 'O':
             return MouthO;
-
-        // Vowel U (OO) - puckered
         case 'U':
-            return MouthQW;
-
+            return MouthU;
         default:
-            return MouthBMP;
+            return MouthSIL;
     }
 }
 
